6ace3b03df07f55c32eff92d826edcd6dab71f97
Sam Cooper <003.sjc@gmail.com>
2025-03-22 14:18:44 +0000

    Make concurrent requests

--- src/http_load_tester/core.clj
+++ src/http_load_tester/core.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:require
    [clj-http.client :as client]
+   [clojure.core.async :refer [<! <!! >! >!! chan go-loop]]
    [clojure.spec.alpha :as s]
    [clojure.string :as str]))
 
@@ -16,26 +17,65 @@
   [url]
   (if (s/valid? ::url-string url)
     (try
-      (client/get url)
+      (let [response (client/get url {:as :stream 
+                                      :retry-handler (fn [& _] false)
+                                      :socket-timeout 5000
+                                      :connection-timeout 3000})]
+        ;; Close the stream and discard the body
+        (when-let [body (:body response)]
+          (.close body))
+        (dissoc response :body))
       (catch Exception e
         {:error (str "Request failed: " (.getMessage e))}))
     {:error (str "Invalid URL: " (s/explain-str ::url-string url))}))
 
 (defn make-requests
-  [{:keys [url request-count]}]
-  (map (fn [_] (get-request url)) (range request-count)))
+  [{:keys [url request-count concurrent-requests]}]
+  (let [request-channel (chan request-count)
+        response-channel (chan request-count)
+        responses (atom [])]
+
+    (dotimes [i request-count]
+      (>!! request-channel i))
+
+    (dotimes [_ concurrent-requests]
+      (go-loop []
+        (when-let [_ (<! request-channel)]
+          (>! response-channel (get-request url)))
+        (recur)))
+
+    (dotimes [_ request-count]
+      (swap! responses conj (<!! response-channel)))
+    
+    @responses))
 
 (defn parse-args
   [& args]
   (try
     (cond
       ;; -u <url>
-      (and (= (first args) "-u") (= 2 (count args)))
-      {:url (second args) :request-count 1}
+      (and (= "-u" (first args))
+           (= 2 (count args)))
+      {:url (second args)
+       :request-count 1
+       :concurrent-requests 1}
 
       ;; -u <url> -n <request-count>
-      (and (= (first args) "-u") (= (nth args 2) "-n") (= 4 (count args)))
-      {:url (second args) :request-count (Integer/parseInt (last args))}
+      (and (= "-u" (first args))
+           (= "-n" (nth args 2))
+           (= 4 (count args)))
+      {:url (second args)
+       :request-count (Integer/parseInt (last args))
+       :concurrent-requests 1}
+
+      ;; -u <url> -n <request-count> -c <concurrent-requests>
+      (and (= "-u" (first args))
+           (= "-n" (nth args 2))
+           (= "-c" (nth args 4))
+           (= 6 (count args)))
+      {:url (second args)
+       :request-count (Integer/parseInt (nth args 3))
+       :concurrent-requests (Integer/parseInt (last args))}
 
       :else
       {:error "Usage: program -u <url> [-n <request-count>]"})
@@ -60,9 +100,11 @@
       (System/exit 1))))
 
 (comment
-  (def example-url  "http://localhost:8000/hello.txt")
-  (-main "-u" example-url)
+  (def example-url  "http://localhost:8000/large_file.bin")
   (-main "-u" example-url "-n" "3")
+  (-main "-u" example-url "-n" "100" "-c" "10")
+  (-main "-u" example-url "-n" "500" "-c" "50")
+  (-main "-u" example-url "-n" "1000" "-c" "100")
 
   (s/valid? ::url-string example-url)
   (s/valid? ::url-string (str/replace example-url ":" ""))
--- start-test-server.sh
+++ start-test-server.sh
@@ -10,20 +10,19 @@ fi
 mkdir -p test-server
 cd test-server
 
-# Create hello.txt if it doesn't exist
-if [ ! -f "hello.txt" ]; then
-  echo "Creating hello.txt..."
-  echo "Gee, that's a lotta load!" > hello.txt
-else
-  echo "hello.txt already exists, skipping."
-fi
-
-# Create index.html if it doesn't exist
 if [ ! -f "index.html" ]; then
   echo "Creating index.html..."
   echo "<html><body><h1>Hello, load tester &#128075;</h1></body></html>" > index.html
 else
   echo "index.html already exists, skipping."
+fi
+
+if [ ! -f "large_file.bin" ]; then
+  echo "Creating a 10MB test file for load testing..."
+  dd if=/dev/urandom of=large_file.bin bs=1M count=10 status=progress
+  echo "Large file created."
+else
+  echo "large_file.bin already exists, skipping."
 fi
 
 echo "Test server files ready in ./test-server"
