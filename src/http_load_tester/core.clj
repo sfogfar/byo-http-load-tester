06017c055634f832e25034342e627dbd8686936a
Sam Cooper <003.sjc@gmail.com>
2025-03-22 13:19:01 +0000

    Make multiple sequential requests

--- src/http_load_tester/core.clj
+++ src/http_load_tester/core.clj
@@ -5,11 +5,12 @@
    [clojure.spec.alpha :as s]
    [clojure.string :as str]))
 
-(s/def ::url-string (s/and string?
-                           #(try
-                              (java.net.URL. %)  ;; Try to create a URL object
-                              (not (str/blank? %))
-                              (catch Exception _ false))))
+(s/def ::url-string
+  (s/and string?
+         #(try
+            (java.net.URL. %)  ;; Try to create a URL object
+            (not (str/blank? %))
+            (catch Exception _ false))))
 
 (defn get-request
   [url]
@@ -20,20 +21,48 @@
         {:error (str "Request failed: " (.getMessage e))}))
     {:error (str "Invalid URL: " (s/explain-str ::url-string url))}))
 
+(defn make-requests
+  [{:keys [url request-count]}]
+  (map (fn [_] (get-request url)) (range request-count)))
+
+(defn parse-args
+  [& args]
+  (try
+    (cond
+      ;; -u <url>
+      (and (= (first args) "-u") (= 2 (count args)))
+      {:url (second args) :request-count 1}
+
+      ;; -u <url> -n <request-count>
+      (and (= (first args) "-u") (= (nth args 2) "-n") (= 4 (count args)))
+      {:url (second args) :request-count (Integer/parseInt (last args))}
+
+      :else
+      {:error "Usage: program -u <url> [-n <request-count>]"})
+    (catch NumberFormatException _e
+      {:error "Error: COUNT must be a number"})))
+
 (defn -main
   [& args]
-  (let [[url] args]
-    (try
-      (cond
-        (= 1 (count args)) (println (str "Response code: " (:status (get-request url))))
-        :else (throw (Exception. "Invalid arguments")))
-      (catch Exception e
-        (println "Error:" (.getMessage e))
-        (System/exit 1)))))
+  (try
+    (let [parsed-args (apply parse-args args)]
+      (if (:error parsed-args)
+        (do
+          (println (:error parsed-args))
+          (System/exit 1))
+        (let [responses (make-requests parsed-args)]
+          (doseq [response responses]
+            (if (:status response)
+              (println (str "Response code: " (:status response)))
+              (println (str "Error! " (:error response))))))))
+    (catch Exception e
+      (println "Error:" (.getMessage e))
+      (System/exit 1))))
 
 (comment
   (def example-url  "http://localhost:8000/hello.txt")
-  (-main example-url)
+  (-main "-u" example-url)
+  (-main "-u" example-url "-n" "3")
 
   (s/valid? ::url-string example-url)
   (s/valid? ::url-string (str/replace example-url ":" ""))
